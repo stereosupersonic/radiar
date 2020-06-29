bb5d560ca5cbfa22ca674c7289a6a9d9301e16a9
michael <m.deimel@miceportal.com>
2020-06-29 19:48:25 +0200

    quick fix

--- app/services/fetch_all_stations.rb
+++ app/services/fetch_all_stations.rb
@@ -1,7 +1,10 @@
 class FetchAllStations
   def call
+    # TODO make this more robuts => put to resque
     Station.where(enabled: true).each do |station|
       CreateTrack.new(station).call
+    rescue => e
+      Rails.logger.error "Error processing station: #{station.name}", e
     end
   end
 end
