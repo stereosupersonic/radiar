11bca1fb7364ccd2fdbef677fbe6112fa34aa845
michael <m.deimel@miceportal.com>
2020-06-29 16:51:41 +0200

    add docker

---
+++ entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Start the run once job.
+echo "Docker container has been started"
+
+# Setup a cron test schedule
+mkdir -p /var/log/
+touch /var/log/cron.log
+
+echo "write whenever cront entries"
+bundle exec whenever --update-crontab
+
+echo "start cron service"
+service cron start
+echo "run migrations"
+bundle exec rails db:migrate
+
+echo "start rails server"
+mkdir -p /app/tmp/pids
+rm -f /app/tmp/pids/server.pid 
+bundle exec rails server -b 0.0.0.0 -p 3000
