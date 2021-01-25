1717375a8d63f40e66aca56b7040b61db2944022
michael <m.deimel@miceportal.com>
2021-01-25 18:46:43 +0100

    rubocopify

--- app/controllers/admin/fetch_track_infos_controller.rb
+++ app/controllers/admin/fetch_track_infos_controller.rb
@@ -1,8 +1,6 @@
 class Admin::FetchTrackInfosController < ApplicationController
-
   def update
     track = Track.find params[:id]
-
 
     GoogleJob.perform_later(track: track, track_info: track.track_info)
     LastfmJob.perform_later(track: track, track_info: track.track_info)
--- app/controllers/admin/ignore_tracks_controller.rb
+++ app/controllers/admin/ignore_tracks_controller.rb
@@ -1,13 +1,12 @@
 class Admin::IgnoreTracksController < ApplicationController
   def update
-      track = Track.find params[:id]
+    track = Track.find params[:id]
 
-     if track&.slug.present?
-       IgnoreTracks.new(slug: track.slug).call
-       redirect_to root_path, notice: "Track: #{track.slug} is ignored"
-     else
+    if track&.slug.present?
+      IgnoreTracks.new(slug: track.slug).call
+      redirect_to root_path, notice: "Track: #{track.slug} is ignored"
+    else
       redirect_to root_path, notice: "No Track found with id: #{params[:id]}"
-     end
-
+    end
   end
 end
